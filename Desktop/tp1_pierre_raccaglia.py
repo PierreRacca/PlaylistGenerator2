62e958b0cada35acadc98d784157d9398529de22
MELANIE <melaniemanguin@gmail.com>
2015-01-06 16:44:03 +0100

    test

--- Desktop/tp1_pierre_raccaglia.py
+++ Desktop/tp1_pierre_raccaglia.py
@@ -1,8 +1,8 @@
 ## EXERCICE 1
 
 etudiants = [
-  ("Nom1", "Pre1", [ ("TDLOG", 12), ("1INFO", 13), ("Anglais", 12) ]),
-  ("Nom2", "Pre2", [ ("IMG", 15), ("Anglais", 11) ])
+  ("Nom1", "Pre1", [ ("TDLOG", 12), ("1INFO", 13), ("Espagnol", 12) ]),
+  ("Nom2", "Pre2", [ ("IMG", 15), ("Espagnol", 11) ])
 ]
 
 prout
@@ -10,10 +10,10 @@ def affsimp(x):
     for k in x:
         for i in k:
          print(i)
-         
+
 def affsimpp(l):
     [print(i) for x in l for i in x]
-             
+
 affsimp(etudiants)
 affsimpp(etudiants)
 
@@ -25,16 +25,16 @@ def dico2(l):
     res={}
     for x in l:
         res(x[0:2])=dict(x[2])
-    return res 
+    return res
 
-    
+
 ## EXERCICE 3
-    
+
 
 moyennes={}
 for x in etudiants:
     moyennes[x[0:2]]=sum([note[1] for note in x[2]])/len(x[2])
-    
+
 ## EXERCICE 4
 
 min_max={}
